a4c3126e49ce89c02eb32dc23ba411c7066359a2
Relative <kezin636@gmail.com>
2024-11-09 12:53:00 +0300

    First successful GDPad Flatpak build on GH Actions!

--- FlatpakScripts/generate_nuget_sources.sh
+++ FlatpakScripts/generate_nuget_sources.sh
@@ -1,4 +1,9 @@
 SCRIPT=$(realpath "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 
+if [ ! -f $SCRIPTPATH/flatpak-dotnet-generator.py ]; then
+    wget https://raw.githubusercontent.com/flatpak/flatpak-builder-tools/master/dotnet/flatpak-dotnet-generator.py -O $SCRIPTPATH/flatpak-dotnet-generator.py
+fi
+
+
 python3 $SCRIPTPATH/flatpak-dotnet-generator.py --dotnet 8 --freedesktop 23.08 $SCRIPTPATH/../nuget-sources.json $SCRIPTPATH/../GDPadDotnet.csproj
